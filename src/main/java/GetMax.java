e9946e1813ba7ec726280ee9ed9b748a3c47e5c2
Carlos Marti <carlos.marti29@yahoo.com>
2024-12-03 18:41:10 +0000

    Completed Lab

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,15 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+
+        int maxVal = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+            if(maxVal < arr[i]){
+                maxVal = arr[i];
+            }
+        }
+        
+        return maxVal;
     }
 }
